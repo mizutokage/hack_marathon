f0e93887b193fee11c3303b81da9c70b5900fded
mizutokage <51310292+mizutokage@users.noreply.github.com>
2021-02-27 10:34:36 +0900

    update

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import mainfunction
+from .views import mainfunction, testfunction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('top/', mainfunction),
+    path('test/', testfunction)
 ]
--- project/views.py
+++ project/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def mainfunction(request):
     return render(request, 'base.html')
+
+def testfunction(request):
+    return render(request, 'profile.html')
